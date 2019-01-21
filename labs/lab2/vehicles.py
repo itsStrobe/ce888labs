101a697109a4dbfee1c78b036f826514b8ea9d25
Zavala Iglesias <jz18436@essex.ac.uk>
2019-01-21 13:10:29 +0000

    Plotting: Save Figures

--- labs/lab2/vehicles.py
+++ labs/lab2/vehicles.py
@@ -18,12 +18,15 @@ old_patch = mpatches.Patch(color='blue', label='Current Fleet')
 plt.scatter(new_idx, new, c='r') # New data blue
 new_patch = mpatches.Patch(color='red', label='New Fleet')
 plt.legend(handles=[old_patch, new_patch])
+plt.savefig('ScatterPlot_Vehicles.png')
 plt.show()
 
 # Histograms
 plt.hist(old, color='b')
 plt.legend(labels=['Current Fleet'])
+plt.savefig('Histogram_Current.png')
 plt.show()
 plt.hist(new, color='r')
 plt.legend(labels=['New Fleet'])
+plt.savefig('Histogram_New.png')
 plt.show()
