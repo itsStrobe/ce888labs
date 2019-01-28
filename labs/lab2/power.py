9085ea71710de19eebd6c69d4ea31bcdf38becf4
Zavala Iglesias <jz18436@essex.ac.uk>
2019-01-28 11:59:23 +0000

    Bug fixes in lab2/power.py . Added lab3 directory

--- labs/lab2/power.py
+++ labs/lab2/power.py
@@ -1,30 +1,29 @@
 import numpy as np
 
-def p_value(sample1, sample2, reps, size, t_obs):
+def p_value(sample1, sample2, reps, size):
     count = 0
     
     sample = np.concatenate((sample1, sample2))
     
     for it in range(reps):
-        s = np.random.choice(sample, size=size*2, replace=False)
-        s1 = s[:size]
-        s2 = s[size:]
+        s = np.random.choice(sample, sample.size * 2, replace=False)
+        s1 = s[:sample.size]
+        s2 = s[sample.size:]
         
         t_perm = s2.mean() - s1.mean()
         
-        if t_perm > t_obs:
+        if t_perm > size:
             count += 1
             
     return count / reps
 
 def power(sample1, sample2, reps, size, alpha):
-    t_obs = sample2.mean() - sample1.mean()
     count = 0
     
     for it in range(reps):
-        s1 = np.random.choice(sample1, size=size, replace=True)
-        s2 = np.random.choice(sample2, size=size, replace=True)
-        p_val = p_value(s1, s2, reps, size, t_obs)
+        s1 = np.random.choice(sample1, sample1.size, replace=True)
+        s2 = np.random.choice(sample2, sample2.size, replace=True)
+        p_val = p_value(s1, s2, reps, size)
         
         if p_val < (1 - alpha):
             count += 1
